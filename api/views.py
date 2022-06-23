2fb4c1f0c8af4d72e12ce4db7cd0ee41db8b6362
Ayusman Samal <samalayusman@gmail.com>
2022-06-23 14:23:44 +0530

    added dummy html

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import SearchSerializer
 
+from .models import Search
 # Create your views here.
+
+class SearchView(generics.ListCreateAPIView):
+    model = Search
+    serializer_class = SearchSerializer
