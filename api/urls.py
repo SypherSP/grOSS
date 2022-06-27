0cd75dbf1c0e450f380b4e4cf9e0a09e6cbdf526
Udbhav Misra <ultimatesicko@kali>
2022-06-27 17:54:36 +0530

    Views updated

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-        path('description', views.des, name='des'),
-        path('genuine', views.genuine, name='genuine'),
+        path('repo-sec', views.repo_sec, name='repo-sec'),
+        path('repo-gen', views.repo_gen, name='repo-gen'),
+        path('description', views.des, name='description'),
+        path('genuineness_check', views.genuine, name='genuineness_check'),
         ]
