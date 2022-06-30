2f2b4305bab2ab75cba40c8df8d5ead02ade92f8
r3z3l <lcs2020047@iiitl.ac.in>
2022-06-30 15:17:01 +0530

    updated urls.py for search and removed some clutter

--- frontend/build/static/js/react-syntax-highlighter_languages_refractor_ini.c86172f4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[6495],{3409:function(t){function n(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=n,n.displayName="ini",n.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.c86172f4.chunk.js.map
